13eccb79db41eddad3404bb9f9485535224493a0
OnlyWooo <dngurwn@gmail.com>
2024-03-14 20:41:41 +0900

    함수안 함수 꺼내기

--- src/navigations/BottomTabStackNavigator.tsx
+++ src/navigations/BottomTabStackNavigator.tsx
@@ -12,27 +12,27 @@ import {ParamListBase, RouteProp} from '@react-navigation/native';
 const BottomTab = createBottomTabNavigator();
 
 const BottomTabStackNavigator = () => {
+  const getTabBarIcon = (name: string, focused: boolean) => {
+    switch (name) {
+      case 'Home':
+        return <Home stroke={focused ? '#fff' : '#C1C1C1C1'} />;
+      case 'Locations':
+        return <Location stroke={focused ? '#fff' : '#C1C1C1C1'} />;
+      case 'Episodes':
+        return <Episode stroke={focused ? '#fff' : '#C1C1C1C1'} />;
+      case 'Settings':
+        return <Setting stroke={focused ? '#fff' : '#C1C1C1C1'} />;
+      default:
+        return null;
+    }
+  };
+
   const getTabOption = (props: {
     route: RouteProp<ParamListBase, string>;
     navigation: any;
   }) => {
     const {route} = props;
     const {name} = route;
-
-    const getTabBarIcon = (name: string, focused: boolean) => {
-      switch (name) {
-        case 'Home':
-          return <Home stroke={focused ? '#fff' : '#C1C1C1C1'} />;
-        case 'Locations':
-          return <Location stroke={focused ? '#fff' : '#C1C1C1C1'} />;
-        case 'Episodes':
-          return <Episode stroke={focused ? '#fff' : '#C1C1C1C1'} />;
-        case 'Settings':
-          return <Setting stroke={focused ? '#fff' : '#C1C1C1C1'} />;
-        default:
-          return null;
-      }
-    };
 
     return {
       tabBarIcon: ({focused}: {focused: boolean}) =>
